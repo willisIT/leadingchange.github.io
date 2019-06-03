a0ffa4cca38780c837c9d343fcf330903229882e
willisIT <williamskyei24@gmail.com>
2019-06-03 19:24:20 +0100

    Immanuel Leadership
    initial commit

---
+++ precache-manifest.ff2fd9f1bd0cda7bc793f6c5cc472461.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf3f9740232411365c6ab4caa68bf3f2",
+    "url": "/img/passion.bf3f9740.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "fd01de480f65d725eca2774e8df9d24b",
+    "url": "/img/about.fd01de48.jpg"
+  },
+  {
+    "revision": "77c37dfa4acc3044fc5d",
+    "url": "/js/chunk-vendors.3d3889ea.js"
+  },
+  {
+    "revision": "4937c01cec98c6ed8a14edf99c0dbccd",
+    "url": "/img/logo.4937c01c.jpg"
+  },
+  {
+    "revision": "5f202c652cef0ff434bb",
+    "url": "/js/app.9a26ce6f.js"
+  },
+  {
+    "revision": "a9b298718012a28fa8544472083acaf6",
+    "url": "/img/asare.a9b29871.jpg"
+  },
+  {
+    "revision": "2bcbeff5ec79c9ed96680e772b2f5670",
+    "url": "/img/profoduru.2bcbeff5.jpg"
+  },
+  {
+    "revision": "9af5f86b514a17ee3f0b96513ddcbdd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e762358b52f39ebdf94ba4ef7a2a945a",
+    "url": "/img/vision.e762358b.jpg"
+  },
+  {
+    "revision": "f7bec7ae5745de7f3d5efebe60ec45e4",
+    "url": "/img/picskids.f7bec7ae.jpg"
+  },
+  {
+    "revision": "4b8c16623d7cc957ec59f23f668c03b4",
+    "url": "/img/lead2.4b8c1662.jpg"
+  },
+  {
+    "revision": "db33591ff93c22a6c7b9995def8c8da5",
+    "url": "/img/lead1.db33591f.jpg"
+  },
+  {
+    "revision": "7176a9ac2f8491d1ea1ee0b0c22fcd68",
+    "url": "/img/john1.7176a9ac.jpg"
+  },
+  {
+    "revision": "0f298da6c83b8b474e59f55aee493f17",
+    "url": "/img/grouppic1.0f298da6.jpg"
+  },
+  {
+    "revision": "e130aef20e2e35148d80b1d514e4ecef",
+    "url": "/img/group.e130aef2.jpg"
+  },
+  {
+    "revision": "77c37dfa4acc3044fc5d",
+    "url": "/css/chunk-vendors.eefe0daf.css"
+  }
+];
